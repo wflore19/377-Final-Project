0cb0710b6bd551a68bf8e564de11ab2031adb162
wflore19 <wflore19@montgomerycollege.edu>
2025-11-26 22:13:57 -0500

    fix: fetch return

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,33 +2,38 @@ const currentPage = window.location.pathname;
 
 if (currentPage === "/") {
     const likeForm = document.querySelector("#like-quote");
-    likeForm.addEventListener("submit", function(event) {
-    event.preventDefault();
 
-    try {
-        const formData = new FormData(event.target);
-        const quoteId = formData.get("quoteId");
-        const quoteText = formData.get("quoteText");
-        const quoteAuthor = formData.get("quoteAuthor");
-    
-        const response = fetch("/quotes/like", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ quoteId: quoteId, quoteText: quoteText, quoteAuthor: quoteAuthor })
+    if (likeForm) {
+        likeForm.addEventListener("submit", async function(event) {
+            event.preventDefault();
+
+            try {
+                const formData = new FormData(event.target);
+                const quoteId = formData.get("quoteId");
+                const quoteText = formData.get("quoteText");
+                const quoteAuthor = formData.get("quoteAuthor");
+            
+                const response = await fetch("/quotes/like", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ quoteId: quoteId, quoteText: quoteText, quoteAuthor: quoteAuthor })
+                });
+
+                const result = await response.json();
+                console.log("Quote successfully liked:", result);
+            
+            } catch (error) {
+                console.error("Error liking quote:", error);
+                console.log("There was an error liking the quote. Please try again.");
+            }
         });
-    
-    } catch (error) {
-        console.error("Error liking quote:", error);
-        console.log("There was an error liking the quote. Please try again.");
     }
-});
 }
 else if (currentPage === "/about") {}
 else if (currentPage === "/project") {}
 else if (currentPage === "/quotes") {
-    console.log("On quotes page");
     const unlikeForm = document.querySelector("#unlike-quote");
     
     if (unlikeForm) {

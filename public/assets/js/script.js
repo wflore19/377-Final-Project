f9f32ada578bb7a28261587fbe17fce5c1630eb0
wflore19 <wflore19@montgomerycollege.edu>
2025-11-27 17:13:53 -0500

    fix: quote functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,8 @@
 const currentPage = window.location.pathname;
 
 if (currentPage === '/') {
-      const likeForm = document.querySelector('#like-quote');
-      const unlikeForm = document.querySelector('#unlike-quote');
-      const submitBtn = document.querySelector('#like-submit-btn');
+      const likeForm = document.querySelector("[id^='like-quote']");
+      const unlikeForm = document.querySelector("[id^='unlike-quote']");
 
       if (likeForm) {
             likeForm.addEventListener('submit', async (event) => {
@@ -14,7 +13,10 @@ if (currentPage === '/') {
                   try {
                         const response = await likeQuote(quoteId);
 
-                        if (response.status === 201) submitBtn.disabled = true;
+                        if (response.status === 201) {
+                              likeForm.remove();
+                              renderUnlikeForm(quoteId);
+                        }
                   } catch (error) {
                         console.error('Error liking quote:', error);
                   }
@@ -30,7 +32,10 @@ if (currentPage === '/') {
                   try {
                         const response = await unlikeQuote(quoteId);
 
-                        if (response.status === 201) submitBtn.disabled = false;
+                        if (response.status === 201) {
+                              unlikeForm.remove();
+                              renderLikeForm(quoteId);
+                        }
                   } catch (error) {
                         console.error(error);
                   }
@@ -50,44 +55,15 @@ if (currentPage === '/') {
             if (unlikeForm) {
                   unlikeForm.addEventListener('submit', async function (event) {
                         event.preventDefault();
+                        const formData = new FormData(event.target);
+                        const quoteId = formData.get('quoteId');
 
                         try {
-                              const formData = new FormData(event.target);
-                              const quoteId = formData.get('quoteId');
-                              const quoteText = formData.get('quoteText');
-                              const quoteAuthor = formData.get('quoteAuthor');
+                              const response = await unlikeQuote(quoteId);
 
-                              const response = await fetch('/quotes/unlike', {
-                                    method: 'DELETE',
-                                    headers: {
-                                          'Content-Type': 'application/json',
-                                    },
-                                    body: JSON.stringify({
-                                          quoteId: quoteId,
-                                          quoteText: quoteText,
-                                          quoteAuthor: quoteAuthor,
-                                    }),
-                              });
-
-                              const result = await response.json();
-                              console.log(
-                                    'Quote successfully unliked:',
-                                    result
-                              );
-
-                              if (result.success) {
-                                    const quoteEl = document.getElementById(
-                                          `quote-${quoteId}`
-                                    );
-                                    if (quoteEl) {
-                                          quoteEl.remove();
-                                    }
-                              }
+                              if (response.status === 201) quoteEl.remove();
                         } catch (error) {
-                              console.error('Error unliking quote:', error);
-                              console.log(
-                                    'There was an error unliking the quote. Please try again.'
-                              );
+                              console.error(error);
                         }
                   });
             }
@@ -144,4 +120,104 @@ async function unlikeQuote(quoteId) {
       } catch (error) {
             throw error;
       }
+}
+
+/** Render like form
+ * @returns {string} HTML for the like form.
+ */
+function renderLikeForm(quoteId) {
+      const formContainerEl = document.getElementById('formContainer');
+
+      const form = document.createElement('form');
+      form.id = 'like-quote';
+      form.method = 'POST';
+
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = 'quoteId';
+      input.value = quoteId;
+      form.appendChild(input);
+
+      const button = document.createElement('button');
+      button.type = 'submit';
+      button.id = 'like-submit-btn';
+      button.textContent = 'Like';
+      form.appendChild(button);
+
+      const icon = document.createElement('img');
+      icon.src = './icons/heart-3-line.svg';
+      icon.alt = 'Like Icon';
+      icon.id = 'like-icon';
+      icon.width = 16;
+      icon.height = 16;
+      button.appendChild(icon);
+
+      form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.target);
+            const quoteId = formData.get('quoteId');
+
+            try {
+                  const response = await likeQuote(quoteId);
+
+                  if (response.status === 201) {
+                        form.remove();
+                        renderUnlikeForm(quoteId);
+                  }
+            } catch (error) {
+                  console.error('Error liking quote:', error);
+            }
+      });
+
+      formContainerEl.appendChild(form);
+}
+
+/** Render unlike form
+ * @returns {string} HTML for the unlike form.
+ */
+function renderUnlikeForm(quoteId) {
+      const formContainerEl = document.getElementById('formContainer');
+
+      const form = document.createElement('form');
+      form.id = `#unlike-quote-${quoteId}`;
+      form.method = 'POST';
+
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = 'quoteId';
+      input.value = quoteId;
+      form.appendChild(input);
+
+      const button = document.createElement('button');
+      button.type = 'submit';
+      button.id = 'unlike-submit-btn';
+      button.textContent = 'Unlike';
+      form.appendChild(button);
+
+      const icon = document.createElement('img');
+      icon.src = './icons/heart-3-fill.svg';
+      icon.alt = 'Unlike Icon';
+      icon.id = 'unlike-icon';
+      icon.width = 16;
+      icon.height = 16;
+      button.appendChild(icon);
+
+      form.addEventListener('submit', async function (event) {
+            event.preventDefault();
+            const formData = new FormData(event.target);
+            const quoteId = formData.get('quoteId');
+
+            try {
+                  const response = await unlikeQuote(quoteId);
+
+                  if (response.status === 201) {
+                        form.remove();
+                        renderLikeForm(quoteId);
+                  }
+            } catch (error) {
+                  console.error(error);
+            }
+      });
+
+      formContainerEl.appendChild(form);
 }

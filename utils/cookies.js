66b31ad33329afe5219b0818f78c8c3d1121a261
wflore19 <wflore19@montgomerycollege.edu>
2025-11-27 12:15:40 -0500

    chore: clean middleware

--- utils/cookies.js
+++ utils/cookies.js
@@ -4,19 +4,18 @@ import supabase from "../utils/database.js";
 async function cookieMiddleware(req, res, next) {
     if (!req.cookies.user_id) {
         const userId = uuidv4();
-    
-        res.cookie('user_id', userId, { maxAge: 365 * 24 * 60 * 60 * 1000, httpOnly: true });
 
-        // save to database
-        const { data, error } = await supabase
+        try {
+            const { data, error } = await supabase
             .from('users')
             .insert([
                 { id: userId },
             ])
             .select()
-    
-    } else {
-        const userId = req.cookies.user_id;
+            res.cookie('user_id', userId, { maxAge: 365 * 24 * 60 * 60 * 1000, httpOnly: true });
+        } catch (error) {
+            console.error("Error creating new user in database:", error);
+        }
     }
     next();
 }

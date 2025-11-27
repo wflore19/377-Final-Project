46c8104565337f8cb88bffc6a61a253ab6783c7e
wflore19 <wflore19@montgomerycollege.edu>
2025-11-26 22:40:27 -0500

    add: form for each quote

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,7 @@ const currentPage = window.location.pathname;
 
 if (currentPage === "/") {
     const likeForm = document.querySelector("#like-quote");
+    const submitBtn = document.querySelector("#like-submit-btn");
 
     if (likeForm) {
         likeForm.addEventListener("submit", async function(event) {
@@ -22,8 +23,10 @@ if (currentPage === "/") {
                 });
 
                 const result = await response.json();
-                console.log("Quote successfully liked:", result);
-            
+
+                if (result.success) {
+                    submitBtn.disabled = true;
+                }
             } catch (error) {
                 console.error("Error liking quote:", error);
                 console.log("There was an error liking the quote. Please try again.");
@@ -34,32 +37,46 @@ if (currentPage === "/") {
 else if (currentPage === "/about") {}
 else if (currentPage === "/project") {}
 else if (currentPage === "/quotes") {
-    const unlikeForm = document.querySelector("#unlike-quote");
-    
-    if (unlikeForm) {
-        unlikeForm.addEventListener("submit", async function(event) {
-            event.preventDefault();
-        
-            try {
-                const formData = new FormData(event.target);
-                const quoteId = formData.get("quoteId");
-                const quoteText = formData.get("quoteText");
-                const quoteAuthor = formData.get("quoteAuthor");
-            
-                const response = fetch("/quotes/unlike", {
-                    method: "DELETE",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({ quoteId: quoteId, quoteText: quoteText, quoteAuthor: quoteAuthor })
-                });
+    const quotesListEl = document.querySelectorAll(".quote");
+    console.log("Quotes list element:", quotesListEl);
 
-                const result = await response.json();
-                console.log("Quote successfully unliked:", result);
-            } catch (error) {
-                console.error("Error unliking quote:", error);
-                console.log("There was an error unliking the quote. Please try again.");
-            }
-        });
-    }
+    quotesListEl.forEach((quoteEl) => {
+        const quoteId = quoteEl.id.split("-")[1];
+        const unlikeForm = document.querySelector(`#unlike-quote-${quoteId}`);
+    
+        if (unlikeForm) {
+            unlikeForm.addEventListener("submit", async function(event) {
+                event.preventDefault();
+            
+                try {
+                    const formData = new FormData(event.target);
+                    const quoteId = formData.get("quoteId");
+                    const quoteText = formData.get("quoteText");
+                    const quoteAuthor = formData.get("quoteAuthor");
+                
+                    const response = await fetch("/quotes/unlike", {
+                        method: "DELETE",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({ quoteId: quoteId, quoteText: quoteText, quoteAuthor: quoteAuthor })
+                    });
+    
+                    const result = await response.json();
+                    console.log("Quote successfully unliked:", result);
+    
+                    if (result.success) {
+                        const quoteEl = document.getElementById(`quote-${quoteId}`);
+                        if (quoteEl) {
+                            quoteEl.remove();
+                        }
+                    }
+    
+                } catch (error) {
+                    console.error("Error unliking quote:", error);
+                    console.log("There was an error unliking the quote. Please try again.");
+                }
+            });
+        }
+    })
 }
